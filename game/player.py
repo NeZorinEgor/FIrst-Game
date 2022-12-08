abff2fe52f3f04d9b932d4de0cdb03e750bd20f6
Svyatoslav Lipatov <angboroda2@mail.ru>
2022-12-08 19:06:05 +0800

    Create player.py

---
+++ game/player.py
@@ -0,0 +1,40 @@
+from settings import *
+import pygame
+import math
+
+
+class Player:
+    def __init__(self):
+        self.x, self.y = player_pos
+        self.angle = player_angle
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
+
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.x += movement_speed * cos_a
+            self.y += movement_speed * sin_a
+            print("W")
+        if keys[pygame.K_s]:
+            self.x += -movement_speed * cos_a
+            self.y += -movement_speed * sin_a
+            print("S")
+        if keys[pygame.K_a]:
+            self.x += movement_speed * sin_a
+            self.y += -movement_speed * cos_a
+            print("A")
+        if keys[pygame.K_d]:
+            self.x += -movement_speed * sin_a
+            self.y += movement_speed * cos_a
+            print("D")
+        if keys[pygame.K_LEFT]:
+            self.angle -= 0.02
+            print("←")
+        if keys[pygame.K_RIGHT]:
+            self.angle += 0.02
+            print("→")
