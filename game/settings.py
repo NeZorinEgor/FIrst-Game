a4965c9763ace3e11b95acaacf19c58400b8b678
Yawnmain <108503319+Yawnmain@users.noreply.github.com>
2022-12-08 19:12:19 +0800

    Настройки PyGame
    Настройки PyGame

---
+++ game/settings.py
@@ -0,0 +1,29 @@
+####################
+
+WIDTH = 1600
+HEIGHT = 900
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+FPS = 120
+BLOCK = 50
+
+####################
+FOV = 3.14 / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = 50
+MAX_DEPTH = 800
+DELTA_ANGLE = FOV / NUM_RAYS
+
+####################
+player_pos = (HALF_WIDTH, HALF_HEIGHT)
+player_angle = 0
+movement_speed = 2
+
+####################
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 0, 0)
+GREEN = (0, 220, 0)
+BLUE = (0, 0, 255)
+LIGHTGRAY = (140, 140, 140)
+
