efc2ed20a149cd1f8cc0648a2751fa6b4ae611b5
Yawnmain <108503319+Yawnmain@users.noreply.github.com>
2023-01-05 20:25:49 +0800

    Корректироовка game

--- game/game.py
+++ game/game.py
@@ -1,16 +1,16 @@
-import math
+
 import sys
 import pygame as pg
 from settings import *
 from player import Player
-from field import world_field
-from raycast import raycast
+from area import Area
 
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
 player = Player()
 running_game = True
+area = Area(screen)
 
 while running_game:
     for event in pg.event.get():
@@ -21,22 +21,23 @@ while running_game:
             if event.key == pg.K_ESCAPE:
                 sys.exit()
 
-
     player.moving()
     screen.fill(WHITE)
 
-    #Добавлено
-    pg.draw.rect(screen, WHITE, (0, 0, WIDTH, HALF_HEIGHT)) #Небо
-    pg.draw.rect(screen, LIGHTGRAY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT)) #Земля
+    # Убрано
+    # pg.draw.rect(screen, WHITE, (0, 0, WIDTH, HALF_HEIGHT)) #Небо
+    # pg.draw.rect(screen, LIGHTGRAY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT)) #Земля
+    # raycast(screen, player.get_position, player.angle) #Отображение лучей
 
-    raycast(screen, player.get_position, player.angle) #Отображение лучей
-
-    #Убрано
+    # Убрано
     # pg.draw.circle(screen, GREEN, (int(player.x), int(player.y)), 12)
     # pg.draw.line(screen, GREEN, player.pos, (player.x + WIDTH * math.cos(player.angle),
     #                                          player.y + WIDTH * math. sin(player.angle)), 2)
     # for x,y in world_field:
     #     pg.draw.rect(screen, BLACK, (x, y, BLOCK, BLOCK), 2)
+
+    area.environment()
+    area.world(player.get_position, player.angle)
 
     pg.display.flip()
     clock.tick(FPS)
