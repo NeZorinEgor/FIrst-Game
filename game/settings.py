162af38254d30cc142c87206e09ba89ca9b1662f
liquikk <107905363+liquikk@users.noreply.github.com>
2023-01-06 22:59:02 +0800

    update settings

--- game/settings.py
+++ game/settings.py
@@ -1,36 +1,43 @@
 import math
-####################
 
-WIDTH = 1600
-HEIGHT = 900
+#_____Настройки_____
+WIDTH = 1200
+HEIGHT = 800
 HALF_WIDTH = WIDTH // 2
 HALF_HEIGHT = HEIGHT // 2
 FPS = 120
 BLOCK = 50
 
-####################
+#_____Настройки игрока_____
+PLAYER_POS = (HALF_WIDTH, HALF_HEIGHT)
+PLAYER_ANGLE = 0
+MOVEMENT_SPEED = 2
+
+#_____Настройки карты_____
+MAP_SCALE = 5
+
+#_____Настройки рейкастинга_____
 FOV = 3.14 / 3
 HALF_FOV = FOV / 2
-NUM_RAYS = 50
+NUM_RAYS = 300
 MAX_DEPTH = 800
 DELTA_ANGLE = FOV / NUM_RAYS
+DISTANCE = NUM_RAYS / (2 * math.tan(HALF_FOV))
+PROJECTION_COEFFICIENT = 3 * DISTANCE * BLOCK
+FOV_SIZE = WIDTH // NUM_RAYS
 
-####################
-player_pos = (HALF_WIDTH, HALF_HEIGHT)
-player_angle = 0
-movement_speed = 2
+#_____Настройки текстур_____
+TEXTURE_WIDTH = 1200
+TEXTURE_HEIGHT = 1200
+TEXTURE_SCALE = TEXTURE_WIDTH // BLOCK
 
-####################
+#_____Цвета_____
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (220, 0, 0)
 GREEN = (0, 220, 0)
 BLUE = (0, 0, 255)
-LIGHTGRAY = (140, 140, 140)
+DARKGRAY = (50, 50, 50)
+LIGHTGRAY = (240, 240, 240)
 SKY = (0, 186, 255)
 FLOOR = (0, 101, 62)
-
-# Добавлено
-DISTANCE = NUM_RAYS / (2 * math.tan(HALF_FOV))
-PROJECTION_COEFFICIENT = 3 * DISTANCE * BLOCK
-FOV_SIZE = WIDTH // NUM_RAYS
