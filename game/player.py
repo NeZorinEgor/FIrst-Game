a6ef2b5c1a0f144e2fe74516f1ac14ad4ab406af
Егор <92092053+NeZorinEgor@users.noreply.github.com>
2023-01-06 23:05:51 +0800

    Update player.py

--- game/player.py
+++ game/player.py
@@ -4,39 +4,33 @@ import math
 
 class Player:
     def __init__(self):
-        self.pos, self.ang = player_pos
-        self.angle = player_angle
+        self.pos, self.ang = PLAYER_POS
+        self.angle = PLAYER_ANGLE
         self.sensivity = 0.004
 
     @property
     def get_position(self):
         return (self.pos, self.ang)
 
-    def movement(self):
-        self.moving()
-        self.mouse_control()
-        self.angle %= DOUBLE_PI
-    
     def moving(self):
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
         keys = pg.key.get_pressed()
-
         if keys[pg.K_w]:
-            self.pos += movement_speed * cos_a
-            self.ang += movement_speed * sin_a
+            self.pos += MOVEMENT_SPEED * cos_a
+            self.ang += MOVEMENT_SPEED * sin_a
             print("W")
         if keys[pg.K_a]:
-            self.pos += movement_speed * sin_a
-            self.ang += -movement_speed * cos_a
+            self.pos += MOVEMENT_SPEED * sin_a
+            self.ang += -MOVEMENT_SPEED * cos_a
             print("A")
         if keys[pg.K_s]:
-            self.pos += -movement_speed * cos_a
-            self.ang += -movement_speed * sin_a
+            self.pos += -MOVEMENT_SPEED * cos_a
+            self.ang += -MOVEMENT_SPEED * sin_a
             print("S")
         if keys[pg.K_d]:
-            self.pos += -movement_speed * sin_a
-            self.ang += movement_speed * cos_a
+            self.pos += -MOVEMENT_SPEED * sin_a
+            self.ang += MOVEMENT_SPEED * cos_a
             print("D")
         if keys[pg.K_LEFT]:
             self.angle -= 0.02
@@ -49,4 +43,4 @@ class Player:
         if pg.mouse.get_focused():
             difference = pg.mouse.get_pos()[0] - HALF_WIDTH
             pg.mouse.set_pos((HALF_WIDTH, HALF_HEIGHT))
-            self.angle += difference * self.sensivity
+            self.angle += difference * self.sensivity 
