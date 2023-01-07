2f31c626f346468a57909abf3512367e048639ef
liquikk <107905363+liquikk@users.noreply.github.com>
2023-01-07 23:43:27 +0800

    update

--- game/area.py
+++ game/area.py
@@ -1,14 +1,14 @@
-import pygame
+import pygame as pg
 from settings import *
-from raycast import raycast
 
 class Area:
     def __init__(self, screen, screen_field):
         self.screen = screen
         self.screen_field = screen_field
-        self.textures = {'|': pygame.image.load('images/texture1.png').convert(),
-                         'I': pygame.image.load('images/texture2.png').convert(),
-                         'SKY': pygame.image.load('images/sky.png').convert()
+
+        self.textures = {'|': pg.image.load('images/texture1.png').convert(),
+                         'I': pg.image.load('images/texture2.png').convert(),
+                         'SKY': pg.image.load('images/skybox.jpg').convert()
                          }
 
     def environment(self, angle):
@@ -16,7 +16,10 @@ class Area:
         self.screen.blit(self.textures['SKY'], (sky_offset, 0))
         self.screen.blit(self.textures['SKY'], (sky_offset - WIDTH, 0))
         self.screen.blit(self.textures['SKY'], (sky_offset + WIDTH, 0))
-        pygame.draw.rect(self.screen, DARKGRAY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
+        pg.draw.rect(self.screen, FLOOR, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
 
-    def world(self, pos, angle):
-        raycast(self.screen, pos, angle, self.textures)
+    def world(self, world_objects):
+        for obj in sorted(world_objects, key=lambda n: n[0], reverse=True):
+            if obj[0]:
+                _, object, object_pos = obj
+                self.screen.blit(object, object_pos)
