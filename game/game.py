9539f97dd6b281462a017a13343c31329c4ddf42
Yawnmain <108503319+Yawnmain@users.noreply.github.com>
2023-01-07 23:40:50 +0800

    Fix game.py

--- game/game.py
+++ game/game.py
@@ -1,19 +1,25 @@
-
 import sys
 import pygame as pg
 from settings import *
+
 from player import Player
+from sprites import *
+from raycast import raycast
 from area import Area
 
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
+
 clock = pg.time.Clock()
 player = Player()
 running_game = True
 
 #Отрисовка местности
-screen_field = pg.Surface((WIDTH // MAP_SCALE, HEIGHT // MAP_SCALE))
+screen_field = pygame.Surface((WIDTH // MAP_SCALE, HEIGHT // MAP_SCALE))
 area = Area(screen, screen_field)
+
+#Спрайты
+sprites = Sprites()
 
 while running_game:
     for event in pg.event.get():
@@ -27,20 +33,9 @@ while running_game:
     player.moving()
     screen.fill(WHITE)
 
-    # Убрано
-    # pg.draw.rect(screen, WHITE, (0, 0, WIDTH, HALF_HEIGHT)) #Небо
-    # pg.draw.rect(screen, LIGHTGRAY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT)) #Земля
-    # raycast(screen, player.get_position, player.angle) #Отображение лучей
-
-    # Убрано
-    # pg.draw.circle(screen, GREEN, (int(player.x), int(player.y)), 12)
-    # pg.draw.line(screen, GREEN, player.pos, (player.x + WIDTH * math.cos(player.angle),
-    #                                          player.y + WIDTH * math. sin(player.angle)), 2)
-    # for x,y in world_field:
-    #     pg.draw.rect(screen, BLACK, (x, y, BLOCK, BLOCK), 2)
-
     area.environment(player.angle)
-    area.world(player.get_position, player.angle)
+    walls = raycast(player, area.textures)
+    area.world(walls + [obj.object_place(player, walls) for obj in sprites.list_of_objects])
 
     pg.display.flip()
     clock.tick(FPS)
