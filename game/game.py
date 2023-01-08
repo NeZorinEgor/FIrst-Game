f4c2d97c700fc5f3ddc2ba7c7cecf5d6b3f6cc85
Svyatoslav Lipatov <angboroda2@mail.ru>
2023-01-09 01:20:58 +0800

    Update game.py

--- game/game.py
+++ game/game.py
@@ -1,42 +1,33 @@
-import sys
-import pygame as pg
+import pygame
 from settings import *
-
 from player import Player
 from sprites import *
 from raycast import raycast
-from area import Area
+from area import Drawing
+from music import Music
 
-pg.init()
-screen = pg.display.set_mode((WIDTH, HEIGHT))
+class Game:
+    def start_game():
+        sc = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.mouse.set_visible(False)
 
-clock = pg.time.Clock()
-pg.mouse.set_visible(False)
-player = Player()
-running_game = True
+        sprites = Sprites()
+        clock = pygame.time.Clock()
+        player = Player(sprites)
+        drawing = Drawing(sc)
+        Music.play_music()
 
-#Отрисовка местности
-screen_field = pygame.Surface((WIDTH // MAP_SCALE, HEIGHT // MAP_SCALE))
-area = Area(screen, screen_field)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+            player.moving()
+            sc.fill(WHITE)
 
-#Спрайты
-sprites = Sprites()
+            drawing.environment(player.angle)
+            walls = raycast(player, drawing.textures)
+            drawing.world(walls + [obj.object_place(player)
+                          for obj in sprites.list_of_objects])
 
-while running_game:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            sys.exit()
-
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_ESCAPE:
-                sys.exit()
-
-    player.moving()
-    screen.fill(WHITE)
-
-    area.environment(player.angle)
-    walls = raycast(player, area.textures)
-    area.world(walls + [obj.object_place(player, walls) for obj in sprites.list_of_objects])
-
-    pg.display.flip()
-    clock.tick(FPS)
+            pygame.display.flip()
+            clock.tick(FPS)
